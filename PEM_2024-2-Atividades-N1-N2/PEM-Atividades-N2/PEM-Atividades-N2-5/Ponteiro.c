ef73095c77bfbbcc3b6ecf7513e1a6c4eb53330c
Aluno <aluno@IPIRANGA.FATEC>
2024-11-01 18:08:25 -0300

    atividade n2-5 incompleta

---
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividades-N2-5/Ponteiro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Produto{
+    int id;
+    char nome[50];
+    int qtde;
+    float valor;
+};
+
+void cadastrar{
+    printf("Digite o ID do novo produto: %d", id);
+    scanf("%d", id);
+    printf("Digite o nome do produto: %s", nome);
+    scanf("%s", str);
+    printf("Digite a quantidade do produto: %d", qtde);
+    scanf("%d", qtde);
+    printf("Digite o valor do produto: %f", valor);
+    scanf("%f", valor);
+};
